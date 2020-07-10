b09573ad42d0ebf67819fa25bb6d9f4699e7c440
Kirk Haines <wyhaines@gmail.com>
2020-07-10 13:11:00 -0600

    Has to be a class.

--- src/minion/util.cr
+++ src/minion/util.cr
@@ -1,5 +1,5 @@
 module Minion
-  module Util
+  class Util
     # Given an array or a string, return a string.
     def string_from_string_or_array(val) : String
       val.is_a?(Array) ? val.as(Array).join : val.as(String)
